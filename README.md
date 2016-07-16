8fc4b8ebc00deac81b08937f829e73a368ad3431
Yann Renaudin <yrenaudin@negotium.com>
2016-07-16 13:24:42 -0400

    - Add script to run on a local docker host - Add script to run from a remote docker host - Test with cross-env library for environment variables settings

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 ### SwarmUI
 
+## Monitor the host engine
 ```bash
 $ npm install
 $ npm start
+```
+
+## Monitor a remote host
+```bash
+$ npm install
+$ npm run remote --docker-host=[host-ip]
 ```
--- app.js
+++ app.js
@@ -3,21 +3,39 @@ var Docker = require('dockerode');
 var _ = require('lodash'); 
 
 var app = express();
-var docker = new Docker({host: 'http://10.0.75.2', port: 2375});
+var docker = getDockerClientFromEnv(process.env);
 
 app.get('/', function (req, res) {
     let containersInfos = [];
     let extractProperties = ['Id','Names','Image','Ports','Command','Status']
     docker.listContainers(function (err, containers) {
-        containers.forEach(function (containerInfo) {
-            containersInfos.push(_.pick(containerInfo,extractProperties))
-        });
-        res.send(containersInfos);
+        if(!err){            
+            containers.forEach(function (containerInfo) {
+                containersInfos.push(_.pick(containerInfo,extractProperties))
+            });
+            res.send(containersInfos);
+        }else{
+            res.send(err);
+        }
     });
     
 
 });
 
 app.listen(3000, function () {
-  console.log('Docker Swarm UI is available on port 3000');
+  console.log('Docker Swarm UI is available on port 3000 !');
 });
+
+function getDockerClientFromEnv(env){
+    let docker={};
+    let host = env.npm_config_docker_host;
+    let port = env.npm_package_config_port || 2375;
+    if(!host){
+        console.log("Connecting the docker client to", "/var/run/docker.sock");
+        docker = new Docker({socketPath: '/var/run/docker.sock'});
+    }else{      
+        console.log("Connecting the docker client to", host);
+        docker = new Docker({host: host, port:port});
+    }
+    return docker;
+}
