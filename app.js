2ea9327e807ff10d0ed452858673ce364f82a81a
Yann Renaudin <renaudin.yann@gmail.com>
2016-07-19 23:18:27 -0400

    - cleaned app.js and moved nodes listing into Api implementation - Added Swagger to document the API (OpenApi language .yaml) - Nodes controller returns the list of current nodes in the cluster

--- app.js
+++ app.js
@@ -1,60 +1,15 @@
-var express = require('express');
-var Docker = require('dockerode');
-var _ = require('lodash');
+var SwaggerExpress = require('swagger-express-mw');
+var app = require('express')();
+var path = require('path');
+module.exports = app;
 
-var app = express();
-var docker = getDockerClientFromEnv(process.env);
+var config = {
+  appRoot: path.join(__dirname, 'src')
+};
 
-app.get('/', function (req, res) {
-    let containersInfos = [];
-    let nodeInfos = [];
-    let extractProperties = ['Id', 'Names', 'Image', 'Ports', 'Command', 'Status']
-    docker.listNodes(function (err, nodes) {
-        if (!err) {
-           /* containers.forEach(function (containerInfo) {
-                containersInfos.push(_.pick(containerInfo, extractProperties))
-            });*/
-             nodes.forEach(function (nodeInfo) {
-                nodeInfos.push(nodeInfo)
-            });
-            res.send(nodeInfos);
-        } else {
-            res.send(err);
-        }
-    });
-
-
+SwaggerExpress.create(config, function(err, swaggerExpress) {
+  if (err) { throw err; }
+  swaggerExpress.register(app);
+  var port = process.env.PORT || 3000;
+  app.listen(port);
 });
-
-app.listen(3000, function () {
-    console.log('Docker Swarm UI is available on port 3000 !');
-});
-
-function getDockerClientFromEnv(env) {
-    let docker = {};
-    let host = env.npm_config_docker_host;
-    let port = env.npm_config_docker_port || 2375;
-    let dockerMachineName = env.npm_config_docker_machine;
-    if (!host) {
-        console.log("Connecting the docker client to", "/var/run/docker.sock");
-        docker = new Docker({ socketPath: '/var/run/docker.sock' });
-    } else {
-        if (!dockerMachineName) {
-            console.log("Connecting the docker client to", host);
-            docker = new Docker({ host: host, port: port });
-        }
-        else {
-            
-          /*  docker = new Docker({
-                protocol: 'https',
-                host: host,
-                port: process.env.DOCKER_PORT || 2375,
-                ca: fs.readFileSync('ca.pem'),
-                cert: fs.readFileSync('cert.pem'),
-                key: fs.readFileSync('key.pem')
-            });*/
-        }
-
-    }
-    return docker;
-}
---
+++ src/api/controllers/tasks.controller.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = {
+  tasks: getAllTasks
+};
+
+/*
+  Functions in a127 controllers used for operations should take two parameters:
+
+  Param 1: a handle to the request object
+  Param 2: a handle to the response object
+ */
+function getAllTasks(req, res) {
+  // variables defined in the Swagger document can be referenced using req.swagger.params.{parameter_name}
+ let tasks = [];
+
+  // this sends back a JSON response which is a single string
+  res.json(tasks);
+}
